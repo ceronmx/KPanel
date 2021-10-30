eea76333d9e98fc49cc568f6b03250df9d9caf15
Daniel Pérez <danielzala.v2@gmail.com>
2021-10-30 13:37:08 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+
+  return(
+    <NavigationContainer>
+      <View></View>
+    </NavigationContainer>
+  )
+}
+
+export default App;
+
+
